8a1c5fbab4fda3a98da43892c91ad91973f3a8e0
Nicolas Holanda <nikolas.nhm14@gmail.com>
2025-10-31 09:43:21 -0300

    Loading DB data

--- src/main/java/com/github/nicolasholanda/spring_jdbc/SpringJdbcApplication.java
+++ src/main/java/com/github/nicolasholanda/spring_jdbc/SpringJdbcApplication.java
@@ -1,7 +1,14 @@
 package com.github.nicolasholanda.spring_jdbc;
 
+import com.github.nicolasholanda.spring_jdbc.model.Order;
+import com.github.nicolasholanda.spring_jdbc.model.User;
+import com.github.nicolasholanda.spring_jdbc.repository.OrderRepository;
+import com.github.nicolasholanda.spring_jdbc.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import java.math.BigDecimal;
 
 @SpringBootApplication
 public class SpringJdbcApplication {
@@ -10,4 +17,21 @@ public class SpringJdbcApplication {
 		SpringApplication.run(SpringJdbcApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner loadData(UserRepository userRepository, OrderRepository orderRepository) {
+		return args -> {
+			for (int i = 1; i <= 10; i++) {
+				User user = new User("User " + i, "user" + i + "@example.com");
+				User savedUser = userRepository.save(user);
+
+				for (int j = 1; j <= 5; j++) {
+					Order order = new Order(
+						BigDecimal.valueOf(100.00 + (i * j)),
+						savedUser
+					);
+					orderRepository.save(order);
+				}
+			}
+		};
+	}
 }
